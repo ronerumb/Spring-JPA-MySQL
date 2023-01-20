f3d43782161ae2adb3ff34edd7d2d2e5de64583a
Roner <ronerumb@gmail.com>
2023-01-19 21:40:33 -0300

    put e delete

--- src/main/java/com/crud/controller/UsuarioController.java
+++ src/main/java/com/crud/controller/UsuarioController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,34 +19,52 @@ import com.crud.repository.UsuarioRepository;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepositorio;
-	
-	
+
 	@GetMapping
-	public ResponseEntity<List<Usuario>> getAll(){
-		return  ResponseEntity.ok().body(usuarioRepositorio.findAll());
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok().body(usuarioRepositorio.findAll());
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Usuario> findById(@PathVariable Integer id){
-		
+	public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
+
 		Usuario u = usuarioRepositorio.findById(id).get();
 		return ResponseEntity.ok().body(u);
-		
+
 	}
-	
-	@PostMapping
-	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj){
-		
-		obj = usuarioRepositorio.save(obj);
-		return ResponseEntity.ok().body(obj);
-		
-	}
-	
 	
 
+
+	@PostMapping
+	public ResponseEntity<Usuario> insert(@RequestBody Usuario obj) {
+
+		obj = usuarioRepositorio.save(obj);
+		return ResponseEntity.ok().body(obj);
+
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void deleteById(@PathVariable Integer id){
+		usuarioRepositorio.deleteById(id);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> update(@RequestBody Usuario obj, @PathVariable Integer id){
+		
+		
+		Usuario usuario = usuarioRepositorio.findById(id).get();
+		usuario.setEmail(obj.getEmail());
+		usuario.setName(obj.getName());
+		usuarioRepositorio.save(usuario);
+		
+		return ResponseEntity.ok().body(usuario);
+		
+		
+		
+	}
 	
 
 }
